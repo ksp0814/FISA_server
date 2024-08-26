f379feb56c22c835606dc80ffea3acdb3926d007
ksp0814 <rkdtpvlf456@naver.com>
2024-08-26 17:53:05 +0900

    로그인 검사 기능 까지만 구현

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4d/c038d08a7563001f1d7ac07bbd6ddf9d
@@ -0,0 +1,5 @@
+package dev.syntax.step05statemanaging.step03practice;
+
+public class PracticeRegisterServlet {
+
+}
---
+++ step02_servlet_syntax/src/main/java/dev/syntax/step05statemanaging/step03practice/model/member.java
@@ -0,0 +1,30 @@
+package dev.syntax.step05statemanaging.step03practice.model;
+
+public class member {
+	private String user_id;
+	private String password;
+	
+	public member(String user_id, String password) {
+		super();
+		this.user_id = user_id;
+		this.password = password;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
